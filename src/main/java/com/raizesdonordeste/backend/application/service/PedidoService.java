7f9f268d859cfa7692395dfe482b34f0c10b29f1
pedromtbarcelos <pedromtbarcelos@gmail.com>
2026-09-21 15:20:48 -0300

    Implementa sistema mock para pagamento

--- src/main/java/com/raizesdonordeste/backend/application/service/PedidoService.java
+++ src/main/java/com/raizesdonordeste/backend/application/service/PedidoService.java
@@ -2,26 +2,28 @@ package com.raizesdonordeste.backend.application.service;
 
 import com.raizesdonordeste.backend.application.exception.EstoqueInsuficienteException;
 import com.raizesdonordeste.backend.application.exception.RecursoNaoEncontradoException;
+import com.raizesdonordeste.backend.domain.*;
 import com.raizesdonordeste.backend.domain.entity.*;
 import com.raizesdonordeste.backend.domain.enums.StatusPedido;
 import com.raizesdonordeste.backend.infrastructure.repository.*;
-import jakarta.transaction.Transactional;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 import java.math.BigDecimal;
 import java.time.LocalDateTime;
 import java.util.Optional;
 
-
 @Service
 @RequiredArgsConstructor
 public class PedidoService {
+
     private final PedidoRepository pedidoRepository;
     private final EstoqueRepository estoqueRepository;
     private final ProdutoRepository produtoRepository;
     private final UnidadeRepository unidadeRepository;
     private final ClienteRepository clienteRepository;
+    private final PagamentoService pagamentoService;
 
     @Transactional
     public Pedido criarPedido(Pedido pedidoSolicitado) {
@@ -69,6 +71,27 @@ public class PedidoService {
         pedidoSolicitado.setStatusPedido(StatusPedido.AGUARDANDO_PAGAMENTO);
         pedidoSolicitado.setDataPedido(LocalDateTime.now());
 
-        return pedidoRepository.save(pedidoSolicitado);
+        Pedido pedidoSalvo = pedidoRepository.save(pedidoSolicitado);
+
+        boolean pagamentoAprovado = pagamentoService.processarPagamento(pedidoSalvo);
+
+        if (pagamentoAprovado) {
+            pedidoSalvo.setStatusPedido(StatusPedido.EM_PREPARO);
+        } else {
+            pedidoSalvo.setStatusPedido(StatusPedido.PAGAMENTO_RECUSADO);
+            estornarEstoque(pedidoSalvo);
+        }
+
+        return pedidoRepository.save(pedidoSalvo);
+    }
+
+    private void estornarEstoque(Pedido pedido) {
+        for (ItemPedido item : pedido.getItens()) {
+            Estoque estoque = estoqueRepository.findByUnidadeIdAndProdutoId(pedido.getUnidade().getId(), item.getProduto().getId())
+                    .orElseThrow(() -> new RecursoNaoEncontradoException("Erro ao localizar estoque para estorno."));
+
+            estoque.setQuantidadeSaldo(estoque.getQuantidadeSaldo() + item.getQuantidade());
+            estoqueRepository.save(estoque);
+        }
     }
 }
