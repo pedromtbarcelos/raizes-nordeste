94c199ab3745c2a33ea4716a965958addb0bbe3a
pedromtbarcelos <pedromtbarcelos@gmail.com>
2026-09-26 00:35:35 -0300

    Corrige e ajusta funcionalidades do backend

--- src/main/java/com/raizesdonordeste/backend/infrastructure/repository/ItemPedidoRepository.java
+++ src/main/java/com/raizesdonordeste/backend/infrastructure/repository/ItemPedidoRepository.java
@@ -1,9 +1,10 @@
 package com.raizesdonordeste.backend.infrastructure.repository;
 
+import com.raizesdonordeste.backend.domain.entity.ItemPedido;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 
 @Repository
-public interface ItemPedidoRepository extends JpaRepository {
+public interface ItemPedidoRepository extends JpaRepository<ItemPedido, Long> {
 }
